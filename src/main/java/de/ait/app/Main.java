e5ef3ec2f3e21910cd4a8ddb52928aa1ecf321f7
Anar Aliyev <132437786+rugbymann@users.noreply.github.com>
2023-05-25 20:00:50 +0200

    init commit

---
+++ src/main/java/de/ait/app/Main.java
@@ -0,0 +1,58 @@
+package de.ait.app;
+
+import de.ait.repositories.UsersRepository;
+import de.ait.repositories.UsersRepositoryListImpl;
+import de.ait.repositories.UsersRepositoryTextFileImpl;
+import de.ait.services.UsersService;
+import de.ait.services.UsersServiceImpl;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UsersRepository usersRepository = new UsersRepositoryTextFileImpl("users.txt");
+        UsersRepository testUserRepository = new UsersRepositoryListImpl();
+        UsersService usersService = new UsersServiceImpl(usersRepository);
+
+        while (true) {
+            System.out.println("1. Вывести имена всех пользователей");
+            System.out.println("2. Вывести фамилию самого взрослого пользователя");
+
+            System.out.println("3. Сохранить нового пользователя");
+            System.out.println("4. Вывести средний возраст всех пользователей");
+            System.out.println("5. Вывести возраст самого высокого человека");
+            System.out.println("6. Вывести имя и фамилию самого низкого человека");
+
+            System.out.println("0. Выход");
+
+            int command = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (command) {
+                case 1:
+                    System.out.println("Выводим имена пользователей...");
+                    List<String> names = usersService.getNames();
+
+                    for (String name : names) {
+                        System.out.println(name);
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Выводим самого взрослого пользователя");
+                    String lastName = usersService.getLastNameOfMostAging();
+                    System.out.println(lastName);
+
+                    break;
+                case 0:
+                    System.out.println("Выход");
+                    System.exit(0);
+                default:
+                    System.out.println("Команда не распознана");
+            }
+        }
+    }
+}
---
+++ src/main/java/de/ait/repositories/UsersRepositoryTextFileImpl.java
@@ -0,0 +1,53 @@
+package de.ait.repositories;
+
+import de.ait.models.User;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersRepositoryTextFileImpl implements UsersRepository {
+
+    private String fileName;
+
+    public UsersRepositoryTextFileImpl(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+
+    @Override
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+
+        try (FileReader fileReader = new FileReader(fileName); 
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                User user = parseLine(line);
+                users.add(user);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Произошла ошибка");
+        }
+
+        return users;
+    }
+
+    private static User parseLine(String line) {
+        String[] parsed = line.split("\\|");
+        String firstName = parsed[0];
+        String lastName = parsed[1];
+        int age = Integer.parseInt(parsed[2]);
+        double height = Double.parseDouble(parsed[3]);
+
+        return new User(
+                firstName, lastName, age, height
+        );
+    }
+}
