9dd9f5798f93aa57f0b88f9a9eb268bf870dc689
MaiiaKatts <mayya.katts@gmail.com>
2023-05-26 01:37:54 +0300

    override the getAverageAge method from the UsersService class to UsersServiceImpl class

--- src/main/java/de/ait/services/UsersServiceImpl.java
+++ src/main/java/de/ait/services/UsersServiceImpl.java
@@ -39,7 +39,15 @@ public class UsersServiceImpl implements UsersService {
 
         return userAge.get(maxAge);
     }
-
+    @Override
+    public Double getAverageAge() {
+        List<User> users = usersRepository.findAll();
+        int totalAge = 0;
+        for (User user : users) {
+            totalAge += user.getAge();
+        }
+        return (double) totalAge / users.size();
+    }
 
     public Integer getAgeOfMostHeight(){
 
