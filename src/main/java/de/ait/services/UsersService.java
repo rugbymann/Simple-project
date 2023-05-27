bc217b769738b0e5a7ded1ca8f02f9a183124964
Anar Aliyev <132437786+rugbymann@users.noreply.github.com>
2023-05-27 19:56:19 +0200

    add void saveNewUser

--- src/main/java/de/ait/services/UsersService.java
+++ src/main/java/de/ait/services/UsersService.java
@@ -1,11 +1,13 @@
 package de.ait.services;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UsersService {
     List<String> getNames();
 
     String getLastNameOfMostAging();
+    void saveNewUser() throws IOException;
 
     Double getAverageAge();
 
