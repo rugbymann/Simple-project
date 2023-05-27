e3735d51f0564b04f64af058db68642bfd4aaaff
Anar Aliyev <132437786+rugbymann@users.noreply.github.com>
2023-05-27 19:51:59 +0200

    add method void save

--- src/main/java/de/ait/repositories/UsersRepository.java
+++ src/main/java/de/ait/repositories/UsersRepository.java
@@ -2,8 +2,10 @@ package de.ait.repositories;
 
 import de.ait.models.User;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UsersRepository {
     List<User> findAll();
+    void save(User user) throws IOException;
 }
